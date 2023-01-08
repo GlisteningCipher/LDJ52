83c61aae052366bd8e810f2e29e8eeb0865c4be3
Justin Law <jlawhome08@gmail.com>
2023-01-08 11:33:43 -0800

    Home Background Graphic
    message board also tweens in after load

--- Assets/Scripts/HomeCanvas.cs
+++ Assets/Scripts/HomeCanvas.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class HomeCanvas : MonoBehaviour
 {
     [SerializeField] Object DragonScene;
+    [SerializeField] RectTransform HomePanel;
+    [SerializeField] float StartingYPos;
+    [SerializeField] float AnimTime = 0.5f;
+
+    void Start()
+    {
+        HomePanel.DOLocalMoveY(HomePanel.localPosition.y, AnimTime).From(StartingYPos);
+    }
 
     public void EnterTheDragon()
     {
