21a18270111618178c3b7d8ae32c3b8296592c1b
Justin Law <jlawhome08@gmail.com>
2023-01-08 15:22:43 -0800

    Sight Upgrade Implemented

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,19 +4,25 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] Globals Globals;
     [SerializeField] CinemachineVirtualCamera PlayerCamera;
     [SerializeField] CinemachineVirtualCamera DragonCamera;
+    [SerializeField] CinemachineBrain Brain;
     [SerializeField] string LookButton = "Jump";
+    [SerializeField] float NormalBlendTime = 1.5f;
+    [SerializeField] float FasterBlendTime = 0.75f;
     bool alerted = false;
 
     public void LookPlayer()
     {
+        Brain.m_DefaultBlend.m_Time = Globals.sightUpgrade ? FasterBlendTime : NormalBlendTime;
         PlayerCamera.Priority = 1;
         DragonCamera.Priority = 0;
     }
 
     public void LookDragon()
     {
+        Brain.m_DefaultBlend.m_Time = Globals.sightUpgrade ? FasterBlendTime : NormalBlendTime;
         PlayerCamera.Priority = 0;
         DragonCamera.Priority = 1;
     }
