c05d6974ca09a230b9d019c4ea5c84ac89e7a16c
ShootDawn <72670900+ShootDawn@users.noreply.github.com>
2023-01-09 09:42:48 +0000

    winSFX

--- Assets/Scripts/WinText.cs
+++ Assets/Scripts/WinText.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
-
+using FMODUnity;
 public class WinText : MonoBehaviour
 {
+    [SerializeField] EventReference WinSFX;
     // Start is called before the first frame update
     void OnEnable()
     {
+        RuntimeManager.PlayOneShot(WinSFX);
         //play WIN SFX HERE
     }
 }
